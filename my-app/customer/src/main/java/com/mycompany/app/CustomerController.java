19287d0db177cdcd4633e672863294a8d286fbe3
Brandon Balkaransingh <brandonbalkaransingh@Brandons-MacBook-Pro.local>
2023-01-28 21:37:07 -0500

    got pgadmin/postgres working

--- my-app/customer/src/main/java/com/mycompany/app/CustomerController.java
+++ my-app/customer/src/main/java/com/mycompany/app/CustomerController.java
@@ -1,6 +1,5 @@
 package com.mycompany.app;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-@AllArgsConstructor
+public record CustomerController(CustomerService customerService) {
 
-
-public class CustomerController{
-
-    private final CustomerService customerService;
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
-        log.info("new Customer registration {}", customerRegistrationRequest);
+        log.info("new customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
 }
--- my-app/customer/src/main/java/com/mycompany/app/CustomerRegistrationRequest.java
+++ my-app/customer/src/main/java/com/mycompany/app/CustomerRegistrationRequest.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
 public record CustomerRegistrationRequest(
-    String firstName,
-    String lastName,
-    String email) {
+        String firstName,
+        String lastName,
+        String email) {
 }
